51feedecccc04fad376369f28c15486436c64c55
mohamedsayedfci <mohamedsayedfci>
2025-12-18 00:23:02 +0200

    clear cache

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $name = "Code with mohran"; 
- 
+    $name = "Code with mohran";
+
     return redirect('/admin');
+});
+Route::get('c-cache', function () {
+    \Illuminate\Support\Facades\Artisan::call('cache:clear');
+    \Illuminate\Support\Facades\Artisan::call('optimize:clear');
+
+    return 'cleared';
 });
